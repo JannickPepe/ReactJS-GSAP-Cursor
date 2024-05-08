cb4a180db322fd499cf2858f0c043489700ca12d
Jannick Pedersen <jannickpedersen@live.dk>
2024-05-08 12:55:35 +0200

    made 2 different scenes, the example and own

---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import Scene1 from '@/components/Scene1';
+import Scene2 from '@/components/Scene2';
+
+export default function Home() {
+  return (
+    <main>
+      {/* <Scene2 /> */}
+      <Scene1 />
+    </main>
+  );
+}
